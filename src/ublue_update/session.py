039f10477500017340db21014c83965ff484142a
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-09-14 23:36:15 -0700

    chore: remove check for key

--- src/ublue_update/session.py
+++ src/ublue_update/session.py
@@ -39,8 +39,7 @@ def get_active_sessions():
         print(loginctl_output)
         session_properties.append(loginctl_output)
     for session_info in session_properties:
-        if "Type" in session_info.keys():
-            graphical = session_info["Type"] == "x11" or session_info["Type"] == "wayland"
-            if graphical and session_info["Active"] == "yes":
+        graphical = session_info["Type"] == "x11" or session_info["Type"] == "wayland"
+        if graphical and session_info["Active"] == "yes":
                 active_sessions.append(session_info)
     return active_sessions
