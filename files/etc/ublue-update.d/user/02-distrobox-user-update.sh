b90fccf327a57408b9a74aedefca6690a268e2b5
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-09-20 12:27:19 -0700

    style: use unset keyword

--- files/etc/ublue-update.d/user/02-distrobox-user-update.sh
+++ files/etc/ublue-update.d/user/02-distrobox-user-update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [ -x /usr/bin/distrobox ]; then
-    SUDO_USER="" # avoid distrobox sudo checks
+    unset SUDO_USER # avoid distrobox sudo checks
     /usr/bin/distrobox upgrade -a
 fi
