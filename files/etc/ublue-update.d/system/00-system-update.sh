c4855ce4931551ef4fc2447e131f0029a3a0cb59
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-09-21 13:20:49 -0700

    feat: add auto-sign image

--- files/etc/ublue-update.d/system/00-system-update.sh
+++ files/etc/ublue-update.d/system/00-system-update.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-/usr/bin/rpm-ostree update
+check_for_rebase() {
+    IMAGE_REF_FILE="$1"
+    LOCAL_IMAGE_REF=$(rpm-ostree status --pending-exit-77 -b --json | jq -r '.deployments[0]["container-image-reference"]')
+    if [[ $LOCAL_IMAGE_REF == "null" ]]; then
+        return
+    fi
+    if [ -f $IMAGE_REF_FILE ]; then
+        LOCAL_IMAGE_REF_UNTAGGED=$(echo $LOCAL_IMAGE_REF | awk -F ":" '{print $1":"$2":"$3}')
+        IMAGE_REF=$(cat $IMAGE_REF_FILE | jq -r '."image-ref"')
+        IMAGE_DEFAULT_TAG=$(cat $IMAGE_REF_FILE | jq -r '."image-default-tag"')
+
+        if [[ $LOCAL_IMAGE_REF_UNTAGGED != $IMAGE_REF ]]; then
+            /usr/bin/rpm-ostree rebase "$IMAGE_REF:$IMAGE_DEFAULT_TAG"
+            exit
+        fi
+    fi
+}
+
+if [ -x /usr/bin/rpm-ostree ]; then
+    IMAGE_REF_FILE='/var/home/user/ublue-image-info.json'
+    check_for_rebase $IMAGE_REF_FILE
+    /usr/bin/rpm-ostree upgrade
+fi
--- src/ublue_update/update_checks/wait.py
+++ src/ublue_update/update_checks/wait.py
@@ -5,7 +5,7 @@ from time import sleep
 
 def transaction():
     """Pull deployment status via rpm-ostree"""
-    rpm_ostree_status = ["rpm-ostree", "status", "--json"]
+    rpm_ostree_status = ["/usr/bin/rpm-ostree", "status", "--json"]
     status = run(rpm_ostree_status, stdout=PIPE)
     """Parse transaction state"""
     return loads(status.stdout)["transaction"]
@@ -14,3 +14,4 @@ def transaction():
 def transaction_wait():
     while transaction() is not None:
         sleep(1)
+
