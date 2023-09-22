bb5fcb47cca8cef7f8f7a0147229e7b37544178b
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-09-21 19:36:47 -0700

    style: reformat shell code to pass shellcheck

--- files/etc/ublue-update.d/system/00-system-update.sh
+++ files/etc/ublue-update.d/system/00-system-update.sh
@@ -3,15 +3,15 @@
 check_for_rebase() {
     IMAGE_REF_FILE="$1"
     LOCAL_IMAGE_REF=$(rpm-ostree status --pending-exit-77 -b --json | jq -r '.deployments[0]["container-image-reference"]')
-    if [[ $LOCAL_IMAGE_REF == "null" ]]; then
+    if [[ "$LOCAL_IMAGE_REF" == "null" ]]; then
         return
     fi
-    if [ -f $IMAGE_REF_FILE ]; then
-        LOCAL_IMAGE_REF_UNTAGGED=$(echo $LOCAL_IMAGE_REF | awk -F ":" '{print $1":"$2":"$3}')
-        IMAGE_REF=$(cat $IMAGE_REF_FILE | jq -r '."image-ref"')
-        IMAGE_DEFAULT_TAG=$(cat $IMAGE_REF_FILE | jq -r '."image-default-tag"')
+    if [ -f "$IMAGE_REF_FILE" ]; then
+        LOCAL_IMAGE_REF_UNTAGGED=$(echo "$LOCAL_IMAGE_REF" | awk -F ":" '{print $1":"$2":"$3}')
+        IMAGE_REF=$(jq -r '."image-ref"' < "$IMAGE_REF_FILE")
+        IMAGE_DEFAULT_TAG=$(jq -r '."image-default-tag"' < "$IMAGE_REF_FILE")
 
-        if [[ $LOCAL_IMAGE_REF_UNTAGGED != $IMAGE_REF ]]; then
+        if [[ "$LOCAL_IMAGE_REF_UNTAGGED" != "$IMAGE_REF" ]]; then
             /usr/bin/rpm-ostree rebase "$IMAGE_REF:$IMAGE_DEFAULT_TAG"
             exit
         fi
@@ -20,6 +20,6 @@ check_for_rebase() {
 
 if [ -x /usr/bin/rpm-ostree ]; then
     IMAGE_REF_FILE='/usr/share/ublue-os/image-info.json'
-    check_for_rebase $IMAGE_REF_FILE
+    check_for_rebase "$IMAGE_REF_FILE"
     /usr/bin/rpm-ostree upgrade
 fi
