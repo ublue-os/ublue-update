49a9f7454d765437cdb9a8289ecd04ee074aba36
Gerald Pinder <gmpinder@gmail.com>
2024-06-15 06:52:06 -0400

    fix: Add try except to log error for clearer reason for failure (#118)

--- src/ublue_update/cli.py
+++ src/ublue_update/cli.py
@@ -254,4 +254,8 @@ def main():
 
     # system checks passed
     log.info("System passed all update checks")
-    run_updates(cli_args.system, system_update_available)
+    try:
+        run_updates(cli_args.system, system_update_available)
+    except Exception as e:
+        log.info(f"Failed to update: {e}")
+        os._exit(1)
